ce6009eee4a4f580f7efece5369f3e27d92f1bfd
Artur Sedlukha <sedlukha@icloud.com>
2022-08-24 21:53:52 +0300

    fix

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,3 @@
-import { MemoryRouterProvider } from 'next-router-mock/MemoryRouterProvider/next-12';
-
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' }
 }
-
-export const decorators = [
-  (Story) => <MemoryRouterProvider><Story/></MemoryRouterProvider>
-];
-
